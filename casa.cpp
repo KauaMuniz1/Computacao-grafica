a7679a3413930c91f16cb6001edebbcb1704f384
kaua <kauamuniz001@gmail.com>
2026-05-20 10:46:47 -0300

    terminando a casa

--- casa.cpp
+++ casa.cpp
@@ -7,11 +7,11 @@
 #include <stdio.h>
 #include <stdlib.h> 
 
-void criar_quadrados(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4, int r,int g, int b ){
+void criar_quadrados(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4, float r,float g, float b ){
     glColor3f(r,g,b);
     glBegin(GL_QUADS);
     glVertex2f(x1,y1);
-    glVertex2f(x1,y2);
+    glVertex2f(x2,y2);
     glVertex2f(x3,y3);
     glVertex2f(x4,y4);
 
@@ -19,23 +19,23 @@ void criar_quadrados(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int
         
 }
 
-void criar_triangulos(int x1, int y1, int x2, int y2, int x3, int y3, int r, int g, int b){
+void criar_triangulos(int x1, int y1, int x2, int y2, int x3, int y3, float r, float g, float b){
     glColor3f(r,g,b);
     glBegin(GL_TRIANGLES);
     glVertex2f(x1,y1);
-    glVertex2f(x1,y2);
+    glVertex2f(x2,y2);
     glVertex2f(x3,y3);
 
     glEnd();
 }
 
-void criar_linhas(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4, int r,int g, int b ){
+void criar_linhas(int x1, int y1, int x2, int y2, float r,float g,float b ){
     glColor3f(r,g,b);
+    glLineWidth(3.0);
     glBegin(GL_LINES);
     glVertex2f(x1,y1);
-    glVertex2f(x1,y2);
-    glVertex2f(x3,y3);
-    glVertex2f(x4,y4);
+    glVertex2f(x2,y2);
+   
 
     glEnd();
         
@@ -53,8 +53,66 @@ void display(void) {
   
   glLoadIdentity();            // Carrega a matriz identidade
 
-  
+
+  glPushMatrix(); 
+    criar_triangulos(-100, 200, -150, 70, -50, 70, 0.61, 0, 1); // criando o triangulo que forma a casa
+  glPopMatrix();
+
+
+  glPushMatrix();
+    criar_quadrados(-150,70, -50, 70, -50, -100, -150, -100,    0, 0, 1); //criando o quadrado em baixo do triangulo
+  glPopMatrix();
+
+
+  glPushMatrix();
+    criar_quadrados(-125, -100, -125, -15, -75, -15, -75, -100, 0, 0, 0); // criando a porta da casa
+  glPopMatrix();
+
+  glPushMatrix();
+    criar_quadrados(-115, -60, -110, -60, -110, -65, -115, -65,  1, 1, 1); // criando a maçaneta
+  glPopMatrix();
+
+  glPushMatrix();
+    criar_quadrados(-100,200,  200, 200,  250, 70, -50, 70, 1, 0, 1); // criando o teto do lado do triangulo
+  glPopMatrix();
+
+  glPushMatrix();
+    criar_quadrados(250, 70,   250, -100,   -50, -100,    -50, 70,    0,1,0); //criando o lado da casa (XD)
+   glPopMatrix(); 
+
+  glPushMatrix();
+    criar_quadrados(-125, -100,   -75, -100,  -85, -150,   -145,  -150,   1, 0, 0);  //criando a calçada
+  glPopMatrix();
+
+  glPushMatrix();
+    criar_quadrados(200, -20,    160, -20,   160, -50, 200, -50, 0, 1, 1 ); //criando janela da direita
+  glPopMatrix();
+
+  glPushMatrix();
+    criar_quadrados(0, -20,   40, -20,   40, -50,   0, -50,    0, 1, 1); //criando a janela da esquerda
+  glPopMatrix();
+
+  glPushMatrix();
+    criar_linhas(160, -35,  200, -35,  1,1,0); //linha horizontal da janela direita
+  glPopMatrix();
+
+  glPushMatrix();
+    criar_linhas(180,-50,   180, -20,   1,1,0); //linha vertical da janela direita
+  glPopMatrix();
+
+  glPushMatrix();
+    criar_linhas(0,  -35,  40, -35,   1, 1, 0); // linha horizontal da janela esquetda;
+  glPopMatrix();
+
+  glPushMatrix();
+    criar_linhas(20, -20,   20, -50,   1, 1, 0);
+  glPopMatrix();
+
+
+
+
   glutSwapBuffers();
+
     
 }
 
@@ -65,7 +123,8 @@ void time_callback(int){
     glutTimerFunc(1000/60, time_callback,0); // chamando a funçao de novo infinitamente (LOL)
 }
 
-void resize(GLsizei w, GLsizei h) {
+
+void resize(GLsizei w, GLsizei h){
   if (h == 0) h = 1;
 
   printf("Tam. janela: (%i,%i)\n", w, h);
@@ -91,7 +150,7 @@ int main(int argc, char *argv[]){
     glutReshapeFunc(resize);
     glutTimerFunc(0, time_callback, 0);
     glutMainLoop();
-
+  
     return 0;
         
 }
