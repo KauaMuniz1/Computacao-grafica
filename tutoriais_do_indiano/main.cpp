28fe5fa5ad4304de62de2e2052ebd56b1880a3ec
kaua <kauamuniz001@gmail.com>
2026-05-26 15:15:19 -0300

    anotações da aula 2

--- tutoriais_do_indiano/main.cpp
+++ tutoriais_do_indiano/main.cpp
@@ -6,10 +6,47 @@
 #include <stdlib.h>
 
 void display(); 
+void reshape(int, int);
 
 void init(){
     //define a cor de limpeza da tela, parametros: (vermelho, verde, azul, alpha)
-    glClearColor(1.0, 1.0, 1.0, 1.0);
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+}
+
+
+
+void display(){
+
+    //Limpa o buffer de cor, pintando a tela com a cor já definida no glClearColor
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    //Reseta a matriz de transformação atual para a identidade
+    glLoadIdentity();
+
+    //draw
+    glBegin(GL_POLYGON);
+
+    glVertex2f(0.0, 5.0);
+    glVertex2f(4.0, -3.0);
+    glVertex2f(-4.0, -3.0);
+
+    //
+    glEnd();
+    //Troca o buffer de trás com o de frente
+    glutSwapBuffers(); 
+}
+
+void reshape(int w, int h){
+    //define a área de pixels da janela onde a opengl vai renderizar
+    glViewport(0,0, w, h);//x,y,largura,altura
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    //define uma projeção ortográfica 2D, e os parametros definem os limites do mundo
+    gluOrtho2D(-10,10,-10,10);
+    glMatrixMode(GL_MODELVIEW);
+   
 }
 
 int main(int argc, char**argv){
@@ -37,24 +74,11 @@ int main(int argc, char**argv){
 
     //registra a função display como callback de renderização (o GLUT vai chamar display sempre que precisar redesenhar a janela)
     glutDisplayFunc(display);
+    
+    glutReshapeFunc(reshape);
+
     init();
-
-
     //inicia o loop principal do GLUT(fica rodando até o programa fechar)
     glutMainLoop();
 }
 
-void display(){
-
-    //Limpa o buffer de cor, pintando a tela com a cor já definida no glClearColor
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    //Reseta a matriz de transformação atual para a identidade
-    glLoadIdentity();
-
-    //draw
-
-
-    //Troca o buffer de trás com o de frente
-    glutSwapBuffers(); 
-}
